c3c4f2e6e2081e16c8bdfa031f49aa7b3505178b
Siddharth <siddharth.lakhara@gmail.com>
2020-06-19 13:37:09 +0530

    Updates

---
+++ precache-manifest.84955c7914501d9dc9d81aa66cf449bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34db99acbd37d821109959fa8d7d1037",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c197a092272385557fd7",
+    "url": "/static/css/main.ca6aa651.chunk.css"
+  },
+  {
+    "revision": "f39ea6fb7ad4fd853da9",
+    "url": "/static/js/2.b2f6b5e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b2f6b5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c197a092272385557fd7",
+    "url": "/static/js/main.985611f2.chunk.js"
+  },
+  {
+    "revision": "4ed9747cd86fe85eb035",
+    "url": "/static/js/runtime-main.6f60a200.js"
+  }
+]);
